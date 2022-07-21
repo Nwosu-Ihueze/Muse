e90b539d5aa5dd3f830fdc5a6028a1905a681afc
jefedcreator <jefiene77@gmail.com>
2022-07-21 17:01:37 +0100

    muse

---
+++ contracts/Muse.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+import "../legacy-contracts/ERC4610.sol";
+import "../legacy-contracts/utils/Counters.sol";
+
+
+contract Muse is ERC4610{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    using Strings for uint256;
+
+    // Optional mapping for token URIs
+    mapping(uint256 => string) private _tokenURIs;
+
+    // Mapping owner address to token count
+    mapping(uint256 => uint256) private _balances;
+
+
+    constructor() ERC4610("Muse", "muse") {}
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "ERC721URIStorage: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function mintNft(string memory uri) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function delegate(uint tokenId) public payable{
+        address owner = ownerOf(tokenId);
+        require(!checkDelegates(msg.sender,tokenId),"asset cannot be delegated more than once");
+        require(_msgSender() != owner, "ERC4610: setDelegator to current owner");
+        require(msg.value == 0.1 ether);
+        _setDelegator(msg.sender, tokenId);
+        _balances[tokenId] += msg.value;
+    }
+
+    function withdraw(uint tokenId) public{
+        address owner = ownerOf(tokenId);
+        require(_exists(tokenId), "ERC721URIStorage: URI set of nonexistent token");
+        require(msg.sender == owner, "ERC4610: setDelegator to current owner");
+        (bool success,) = msg.sender.call{value:_balances[tokenId]}("");
+        require(success,"withdraw failed");
+    }
+}
